b6b1eae885f5bc26d2b9aed313e9309ce7d93877
Owen <owen.herbert@hotmail.com>
2023-06-17 10:23:04 +1000

    Add API routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Resources\AgentResource;
+use App\Http\Resources\CountryResource;
+use App\Http\Resources\PropertyResource;
+use App\Models\Agent;
+use App\Models\Country;
+use App\Models\Property;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +22,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('agents', function () {
+    return new AgentResource(Agent::paginate());
+});
+
+Route::get('agents/{id}', function (string $id) {
+    return new AgentResource(Agent::findOrFail($id));
+});
+
+Route::get('properties', function () {
+    return new PropertyResource(Property::paginate());
+});
+
+Route::get('properties/{id}', function (string $id) {
+    return new PropertyResource(Property::findOrFail($id));
+});
+
+Route::get('countries', function () {
+    return new CountryResource(Country::paginate());
+});
+
+Route::get('countries/{id}', function (string $id) {
+    return new CountryResource(Country::findOrFail($id));
 });
