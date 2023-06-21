91797b94323dfc87dca71f3d838666aa0011d493
Owen <owen.herbert@hotmail.com>
2023-06-21 18:04:14 +1000

    Add Spinner component to AgentCard component

--- src/resources/js/components/AgentCard.tsx
+++ src/resources/js/components/AgentCard.tsx
@@ -5,6 +5,7 @@ import StringHelper from "../helpers/StringHelper";
 import Avatar from "./Avatar";
 import moment from "moment";
 import BadgeOutline from "./BadgeOutline";
+import Spinner from "./Spinner";
 
 interface Props {
     agentId: number;
@@ -12,50 +13,54 @@ interface Props {
 
 const AgentCard: React.FC<Props> = (props: Props) => {
     const [agent, setAgent] = useState<Agent>(undefined);
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
 
     useEffect(() => {
-        Api.getAgent(props.agentId).then((response) => {
-            if (response && response.status === Api.STATUS_OK) {
-                setAgent(response.data.data);
-            }
-        });
+        setIsLoading(true);
+        Api.getAgent(props.agentId)
+            .then((response) => {
+                if (response && response.status === Api.STATUS_OK) {
+                    setAgent(response.data.data);
+                }
+            })
+            .finally(setIsLoading(false));
     }, [props]);
 
     const lastSeenAt = agent ? moment(agent.last_seen_at) : undefined;
 
+    // if the spinner should be displayed
+    const shouldDisplaySpinner = !agent || !lastSeenAt || isLoading;
+
+    if (shouldDisplaySpinner) return <Spinner />;
+
     return (
         <React.Fragment>
-            {/* Property Card */}
-            {agent && lastSeenAt && (
-                <div className="bg-base-100 text-sm w-full shadow">
-                    <div className="card-body">
-                        <div className="flex">
-                            <div className="mr-3">
-                                <Avatar imageUrl={agent.image_url} size="sm" />
-                            </div>
-                            <div className="card-title text-primary">
-                                {StringHelper.ucFirst(agent.first_name)}{" "}
-                                {StringHelper.ucFirst(agent.last_name)}
-                            </div>
+            {/* Agent Card */}
+            <div className="bg-base-100 text-sm w-full shadow">
+                <div className="card-body">
+                    <div className="flex">
+                        <div className="mr-3">
+                            <Avatar imageUrl={agent.image_url} size="sm" />
                         </div>
-                        <div className="mb-2">
-                            <p>
-                                {StringHelper.ucFirst(agent.first_name)} was
-                                last online{" "}
-                                <a className="underline">
-                                    {lastSeenAt.fromNow()}
-                                </a>
-                                .
-                            </p>
-                        </div>
-                        <p>Contact Information:</p>
-                        <div className="card-actions">
-                            <BadgeOutline text={agent.mobile_number} />
-                            <BadgeOutline text={agent.email_address} />
+                        <div className="card-title text-primary">
+                            {StringHelper.ucFirst(agent.first_name)}{" "}
+                            {StringHelper.ucFirst(agent.last_name)}
                         </div>
                     </div>
+                    <div className="mb-2">
+                        <p>
+                            {StringHelper.ucFirst(agent.first_name)} was last
+                            online{" "}
+                            <a className="underline">{lastSeenAt.fromNow()}</a>.
+                        </p>
+                    </div>
+                    <p>Contact Information:</p>
+                    <div className="card-actions">
+                        <BadgeOutline text={agent.mobile_number} />
+                        <BadgeOutline text={agent.email_address} />
+                    </div>
                 </div>
-            )}
+            </div>
         </React.Fragment>
     );
 };
