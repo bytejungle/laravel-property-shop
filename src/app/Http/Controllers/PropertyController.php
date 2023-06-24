bad0b837014abc4417239f5a70b8d291886005ee
Owen <owen.herbert@hotmail.com>
2023-06-24 11:26:49 +1000

    Add property filtering to pagination

--- src/app/Http/Controllers/PropertyController.php
+++ src/app/Http/Controllers/PropertyController.php
@@ -13,7 +13,22 @@ class PropertyController extends Controller
     // paginate properties
     public function paginateProperties() {
         $paginationSize = request()->get('size') ?? 6;
-        return new PropertyResource(Property::paginate($paginationSize));
+
+        // get query parameters
+        $countryId = request()->get('country') ?? null;
+        $bedroomCount = request()->get('bedroom_count') ?? 0;
+        $bathroomCount = request()->get('bathroom_count') ?? 0;
+        $carSpaceCount = request()->get('car_count') ?? 0;
+
+        $properties = Property::where('bedroom_count', '>=', $bedroomCount)
+            ->where('bathroom_count', '>=', $bathroomCount)
+            ->where('car_count', '>=', $carSpaceCount)
+            ->when($countryId, function ($query, $countryId) {
+                $query->where('country_id', '=', $countryId);
+            })
+            ->paginate($paginationSize);
+
+        return new PropertyResource($properties);
     }
 
     // get property by id
